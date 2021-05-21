8e57cf29f0715b234058ca959ff6aa617e8430a6
Getpriyanka-20 <70354405+Getpriyanka-20@users.noreply.github.com>
2021-05-21 16:47:14 -0700

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,12 @@ def main():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
-  quotes = f.readlines()
+  quotes = f.read()
   f.close()
 
-  last = 13
-  rnd = random.randint(0, last)
-  print(quotes[13])
+  #last = 13
+  #rnd = random.randint(0, last)
+  print(quotes[3:])
   
 if __name__ == "__main__":
  main()  
